53455f9e50717cbee6949fdb01c462b3450f864e
tohu.masao <konoe_mario@yahoo.co.jp>
2019-08-23 17:25:27 +0900

    page・templateに切り出す

---
+++ src/page/WordList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainTemplate from '../template/Main'
+
+const WordList: React.FC = () => (
+  <MainTemplate>
+    <div>WordList</div>
+  </MainTemplate>
+)
+
+export default WordList
---
+++ src/template/Main.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+/** @jsx jsx */
+import { css, jsx, Global } from '@emotion/core'
+import { Color } from '../const'
+
+const globalStyle = css`
+  body {
+    margin: 0;
+  }
+`
+
+const headerHeight = 60
+
+const headerStyle = css({
+  position: 'fixed',
+  top: 0,
+  height: `${headerHeight}px`,
+  width: '100%',
+  backgroundColor: Color.BACKGROUND_HEADER,
+})
+
+const mainStyle = css({
+  marginTop: `${headerHeight}px`,
+})
+
+const footerStyle = css({
+  position: 'fixed',
+  bottom: 0,
+  height: '95px',
+  width: '100%',
+  backgroundColor: Color.BACKGROUND_FOOTER,
+})
+
+const Main: React.FC = ({ children }) => {
+  return (
+    <div>
+      <Global styles={globalStyle} />
+      <header css={headerStyle}>header</header>
+      <main css={mainStyle}>{children}</main>
+      <footer css={footerStyle}>footer</footer>
+    </div>
+  )
+}
+
+export default Main
