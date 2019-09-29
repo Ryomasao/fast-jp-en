42181e891c6f7462d6bba3a2565f3eccdc5035c0
tohu.masao <konoemario@gmail.com>
2019-09-29 22:45:42 +0900

    add style

--- src/page/TopPage/TopPageContainer.tsx
+++ src/page/TopPage/TopPageContainer.tsx
@@ -1,15 +1,43 @@
 import React from 'react'
 import MainTemplate from 'components/template/Main'
-import { Link } from 'react-router-dom'
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
 
 const TopPageContainer: React.FC = () => (
   <MainTemplate>
-    <div>TopPage</div>
-    <ul>
-      <Link to="/sentences">sentences</Link>
-      <Link to="/admin/sentences">add sentences</Link>
-    </ul>
+    <div css={ContainerStyle}>
+      <button css={ButtonStyle} type="button">
+        学ぶ
+      </button>
+      <button css={ButtonStyle} type="button">
+        例文を追加する
+      </button>
+    </div>
   </MainTemplate>
 )
 
 export default TopPageContainer
+
+const ContainerStyle = css({
+  display: 'flex',
+  height: '100%',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+type ImageProps = {
+  color: string
+}
+
+const ButtonStyle = css({
+  display: 'block',
+  fontSize: '2rem',
+  padding: '1.4rem 2.4rem',
+  width: '50%',
+  margin: '1rem',
+  border: '1px solid transparent',
+  borderRadius: `0.4rem`,
+  backgroundColor: '#FF4641',
+  color: '#fff',
+})
