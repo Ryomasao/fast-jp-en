091813ea72ed950f0761069284bc4a6970cd2fdb
tohu.masao <konoe_mario@yahoo.co.jp>
2019-10-07 10:47:43 +0900

    RouterにSwitchとexactをつける

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import TopPage from 'page/TopPage'
 import WordListPage from 'page/WordListPage'
 import RegisterPgae from 'page/RegisterPage'
 
+// 毎回忘れる
+// exact: これがないと、 /sentences としたいところ、 /sentences/hoge でもマッチすることになる
+// Switch: v4から、pathにマッチするRouteを全部表示するようになってる。大抵は排他的にしたいのでSwitchをつける
 const AppRoute: React.FC = () => (
   <Router>
-    <Route exact path="/" component={TopPage} />
-    <Route path="/sentences" component={WordListPage} />
-    <Route path="/admin/sentences" component={RegisterPgae} />
+    <Switch>
+      <Route exact path="/" component={TopPage} />
+      <Route exact path="/sentences" component={WordListPage} />
+      <Route exact path="/admin/sentences" component={RegisterPgae} />
+    </Switch>
   </Router>
 )
 
